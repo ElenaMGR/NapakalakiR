ce846893885edc93c02d0063566192d013477e4b
joselmo <joselmo@correo.ugr.es>
2015-10-18 17:47:45 +0200

    Creado clase Prize

---
+++ NapakalakiR/lib/prize.rb
@@ -0,0 +1,26 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class Prize
+  
+  # Constructor
+  def initialize( treasures, level)
+    @treasures = treasures
+    @level = level
+  end
+  
+  # Consultores
+  
+  def treasures
+    @treasures
+  end
+  
+  def level
+    @level
+  end
+  
+  
+  
+  
+end
