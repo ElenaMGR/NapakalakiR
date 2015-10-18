9fe8c95807bc49ad5bec813838282e81cf9d49f1
joselmo <joselmo@correo.ugr.es>
2015-10-18 18:27:20 +0200

    Modificado prueba en el main

--- NapakalakiR/lib/prueba_napakalaki.rb
+++ NapakalakiR/lib/prueba_napakalaki.rb
@@ -3,9 +3,11 @@
 # and open the template in the editor.
 
 puts "Hello World"
+texto="Jose"
+puts texto
+#premio=Prize.new(2,1)
+#puts prize1.level
+#puts prize1.treasure
 
-prize1 = Prize.new(2, 1)
-puts prize1.level
-puts prize1.treasure
-
- 
+bc1 = BadConsequence.new('hola',1,0,1,0,0,0)
+puts bc1
