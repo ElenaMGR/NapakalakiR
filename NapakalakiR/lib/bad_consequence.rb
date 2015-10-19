f6519e5f0b276518ff8fb893bde33689c0a0edea
ElenaMGR <elenamgori@gmail.com>
2015-10-19 18:40:59 +0200

    Modificada la clase Bad_consequence

--- NapakalakiR/lib/bad_consequence.rb
+++ NapakalakiR/lib/bad_consequence.rb
@@ -10,6 +10,7 @@ class BadConsequence
   attr_reader :someSpecificVisibleTreasures
   attr_reader :someSpecificHiddenTreasures
   attr_reader :death
+  private_class_method :new
   
   def initialize(aText, someLevels, someVisibleTreasures, someHiddenTreasures, someSpecificVisibleTreasures, someSpecificHiddenTreasures, death)
     @aText=aText
@@ -19,5 +20,45 @@ class BadConsequence
     @someSpecificVisibleTreasures=someSpecificVisibleTreasures
     @someSpecificHiddenTreasures=someSpecificHiddenTreasures
     @death=death
+  end
+  
+  def BadConsequence.newLevelNumberOfTreasures (aText, someLevels, someVisibleTreasures, 
+      someHiddenTreasures)
+    @aText=aText
+    @death=false
+    @someLevels=someLevels
+    @someVisibleTreasures=someVisibleTreasures
+    @someHiddenTreasures=someHiddenTreasures
+    @someSpecificVisibleTreasures=nil
+    @someSpecificHiddenTreasures=nil
+  end
+
+  def BadConsequence.newLevelSpecificTreasures (aText, someLevels, 
+      someSpecificVisibleTreasures, someSpecificHiddenTreasures)
+    @aText=aText
+    @death=false
+    @someLevels=someLevels 
+    @someSpecificVisibleTreasures=someSpecificVisibleTreasures
+    @someSpecificHiddenTreasures=someSpecificHiddenTreasures
+    if (someSpecificVisibleTreasures!=nil)
+      @someVisibleTreasures=someSpecificVisibleTreasures.size
+    else
+      @someVisibleTreasures=0
+    end
+    if (someSpecificHiddenTreasures!=nil)
+      @someHiddenTreasures=someSpecificHiddenTreasures.size
+    else
+      @someHiddenTreasures=0
+    end
+  end
+
+  def BadConsequence.newDeath (aText)
+    @aText=aText
+    @death=true
+    @someLevels=0
+    @someVisibleTreasures=0
+    @someHiddenTreasures=0
+    @someSpecificVisibleTreasures=nil
+    @someSpecificHiddenTreasures=nil
   end
 end
