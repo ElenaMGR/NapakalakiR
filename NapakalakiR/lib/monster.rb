db15887b54701d64eb5109324f8cab3e87cf33d2
joselmo <joselmo@correo.ugr.es>
2015-10-26 10:15:39 +0100

    Arreglado metodo t_s a Monsters

--- NapakalakiR/lib/monster.rb
+++ NapakalakiR/lib/monster.rb
@@ -17,7 +17,7 @@ class Monster
   end
   
   def to_s
-    "Nombre: #{@aText}, CombatLevel: #{@combatlevel} \n Mal rollo: #{@bc.to_s},
+    "Nombre: #{@name}, CombatLevel: #{@combatlevel} \n Mal rollo: #{@bc.to_s},
     Buen rollo: #{@prize.to_s}"
   end
 end
