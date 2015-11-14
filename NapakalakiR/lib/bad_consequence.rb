d9bdab041f4b429e68ae2c0a0c5879ff3651b10b
ElenaMGR <elenamgori@gmail.com>
2015-11-14 21:28:56 +0100

    bad_consequence: modificada como en el diagrama de clases

--- NapakalakiR/lib/bad_consequence.rb
+++ NapakalakiR/lib/bad_consequence.rb
@@ -7,41 +7,62 @@ require_relative 'treasure_kind'
 class BadConsequence
   #attr_accessor
   #attr_writter
-  attr_reader :aText
-  attr_reader :someLevels
-  attr_reader :someVisibleTreasures
-  attr_reader :someHiddenTreasures
-  attr_reader :someSpecificVisibleTreasures
-  attr_reader :someSpecificHiddenTreasures
+  attr_reader :text
+  attr_reader :levels
+  attr_reader :nVisibleTreasures
+  attr_reader :nHiddenTreasures
+  attr_reader :specificVisibleTreasures
+  attr_reader :specificHiddenTreasures
   attr_reader :death
   private_class_method :new
   
-  def initialize(aText, someLevels, someVisibleTreasures, someHiddenTreasures, someSpecificVisibleTreasures, someSpecificHiddenTreasures, death)
-    @aText=aText
-    @someLevels=someLevels
-    @someVisibleTreasures=someVisibleTreasures
-    @someHiddenTreasures=someHiddenTreasures
-    @someSpecificVisibleTreasures=someSpecificVisibleTreasures
-    @someSpecificHiddenTreasures=someSpecificHiddenTreasures
+  public @@MAXTREASURES = 10
+  
+  def initialize(t, l, nVisible, nHidden, v, h, death)
+    @text=t
+    @levels=l
+    @nVisibleTreasures=nVisible
+    @nHiddenTreasures=nHidden
+    @specificVisibleTreasures=v
+    @specificHiddenTreasures=h
     @death=death    
   end
   
-  def self.newLevelNumberOfTreasures (aText, someLevels, someVisibleTreasures, 
-      someHiddenTreasures)
-    new(aText, someLevels, someVisibleTreasures,someHiddenTreasures,Array.new,Array.new,false)
+  def self.newLevelNumberOfTreasures (t, l, nVisible, nHidden)
+    new(t, l, nVisible,nHidden,Array.new,Array.new,false)
   end
 
-  def self.newLevelSpecificTreasures (aText, someLevels, 
-      someSpecificVisibleTreasures, someSpecificHiddenTreasures)
+  def self.newLevelSpecificTreasures (t, l, v, h)
     
-    new(aText,someLevels,0,0,someSpecificVisibleTreasures,someSpecificHiddenTreasures,false)
+    new(t,l,0,0,v,h,false)
    
   end
   
-  def self.newDeath (aText)
-     new(aText,0,0,0,Array.new,Array.new,true)
+  def self.newDeath (t)
+     new(t,0,0,0,Array.new,Array.new,true)
 
   end
+  
+  
+  def isEmpty ()
+    
+  end
+  
+ 
+  def substractVisibleTreasure (t)
+    
+  end
+  
+  
+  def substractHiddenTreasure (t)
+    
+  end
+  
+  
+  def adjustToFitTreasureLists (v,h)
+    
+  end
+  
   
   def to_s
     "Mal rollo: #{@aText} \n Niveles perdidos: #{@someLevels}, Tesoros visibles: #{@someVisibleTreasures}, Tesoros ocultos: #{@someHiddenTreasures},Tesoros específicos visibles: #{@someSpecificVisibleTreasures}, Tesoros específicos ocultos: #{@someSpecificHiddenTreasures}, Muerto: #{@death}."
