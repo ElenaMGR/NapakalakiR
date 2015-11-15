4fb473a56f954e195cffc0daf73d91c5d04e25b9
ElenaMGR <elenamgori@gmail.com>
2015-11-15 12:33:23 +0100

    Treasure: creada segun diagrama de clases

---
+++ NapakalakiR/lib/treasure.rb
@@ -0,0 +1,21 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+
+module NapakalakiGame
+  
+class Treasure
+  
+  # Consultores de acceso
+  attr_reader :name, :bonus, :type
+  
+  # Constructor
+  def initialize (n, bonus, t)
+    @name = n
+    @bonus = bonus
+    @type = t
+  end
+end
+
+end
