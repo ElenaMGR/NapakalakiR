dcafa01cf6aaccaf3fb5768d2f75223e3b3ca396
ElenaMGR <elenamgori@gmail.com>
2015-11-15 12:03:02 +0100

    Prize: modificado segun diagrama de clases

--- NapakalakiR/lib/prize.rb
+++ NapakalakiR/lib/prize.rb
@@ -2,19 +2,26 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-class Prize
-  # Consultores de acceso
-  attr_reader :treasures
-  attr_reader :level
-  
-  
-  # Constructor
-  def initialize( treasures, level)
-    @treasures = treasures
-    @level = level
+module NapakalakiGame
+
+  class Prize
+    # Consultores de acceso
+    attr_reader :treasures
+    attr_reader :levels
+
+
+    # Constructor
+    def initialize( t, l)
+      @treasures = t
+      @level = l
+    end
+    
+    
+    def to_s
+      "Buen rollo: \n Tesoros ganados: #{@treasures}, Niveles ganados: #{@levels}"
+    end
+
   end
-  def to_s
-    "Buen rollo: \n Tesoros ganados: #{@treasures}, Niveles ganados: #{@level}"
-  end
-  
+
+
 end
