3b2500f19928e6b6b1ee322f095854014f4e64ac
ElenaMGR <elenamgori@gmail.com>
2015-11-15 12:22:05 +0100

    Dice : modificado segun diagrama de clases

---
+++ NapakalakiR/lib/dice.rb
@@ -0,0 +1,21 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+require 'singleton'
+
+module NapakalakiGame
+  
+  class Dice
+    
+    include Singleton
+    
+    # Genera un número aleatorio entre 1 y 6 
+    def nextNumber
+      return 1 + rand(6)
+    end
+    
+    
+  end
+
+end
