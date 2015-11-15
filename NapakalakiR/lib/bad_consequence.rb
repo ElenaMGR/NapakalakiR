26bf026b49018d6bf5c6a63d8c799f137a1bb9e6
ElenaMGR <elenamgori@gmail.com>
2015-11-15 11:54:56 +0100

    BadConsequence, Monster: añadido module NapakalakiGame

--- NapakalakiR/lib/bad_consequence.rb
+++ NapakalakiR/lib/bad_consequence.rb
@@ -4,67 +4,71 @@
 # and open the template in the editor.
 require_relative 'treasure_kind'
 
-class BadConsequence
-  #attr_accessor
-  #attr_writter
-  attr_reader :text
-  attr_reader :levels
-  attr_reader :nVisibleTreasures
-  attr_reader :nHiddenTreasures
-  attr_reader :specificVisibleTreasures
-  attr_reader :specificHiddenTreasures
-  attr_reader :death
-  private_class_method :new
-  
-  public @@MAXTREASURES = 10
-  
-  def initialize(t, l, nVisible, nHidden, v, h, death)
-    @text=t
-    @levels=l
-    @nVisibleTreasures=nVisible
-    @nHiddenTreasures=nHidden
-    @specificVisibleTreasures=v
-    @specificHiddenTreasures=h
-    @death=death    
-  end
-  
-  def self.newLevelNumberOfTreasures (t, l, nVisible, nHidden)
-    new(t, l, nVisible,nHidden,Array.new,Array.new,false)
+module NapakalakiGame
+
+  class BadConsequence
+    #attr_accessor
+    #attr_writter
+    attr_reader :text
+    attr_reader :levels
+    attr_reader :nVisibleTreasures
+    attr_reader :nHiddenTreasures
+    attr_reader :specificVisibleTreasures
+    attr_reader :specificHiddenTreasures
+    attr_reader :death
+    private_class_method :new
+
+    public @@MAXTREASURES = 10
+
+    def initialize(t, l, nVisible, nHidden, v, h, death)
+      @text=t
+      @levels=l
+      @nVisibleTreasures=nVisible
+      @nHiddenTreasures=nHidden
+      @specificVisibleTreasures=v
+      @specificHiddenTreasures=h
+      @death=death    
+    end
+
+    def self.newLevelNumberOfTreasures (t, l, nVisible, nHidden)
+      new(t, l, nVisible,nHidden,Array.new,Array.new,false)
+    end
+
+    def self.newLevelSpecificTreasures (t, l, v, h)
+
+      new(t,l,0,0,v,h,false)
+
+    end
+
+    def self.newDeath (t)
+       new(t,0,0,0,Array.new,Array.new,true)
+
+    end
+
+
+    def isEmpty ()
+
+    end
+
+
+    def substractVisibleTreasure (t)
+
+    end
+
+
+    def substractHiddenTreasure (t)
+
+    end
+
+
+    def adjustToFitTreasureLists (v,h)
+
+    end
+
+
+    def to_s
+      "Mal rollo: #{@text} \n Niveles perdidos: #{@levels}, Tesoros visibles: #{@nVisibleTreasures}, Tesoros ocultos: #{@nHiddenTreasures},Tesoros específicos visibles: #{@specificVisibleTreasures}, Tesoros específicos ocultos: #{@specificHiddenTreasures}, Muerto: #{@death}."
+    end
   end
 
-  def self.newLevelSpecificTreasures (t, l, v, h)
-    
-    new(t,l,0,0,v,h,false)
-   
-  end
-  
-  def self.newDeath (t)
-     new(t,0,0,0,Array.new,Array.new,true)
-
-  end
-  
-  
-  def isEmpty ()
-    
-  end
-  
- 
-  def substractVisibleTreasure (t)
-    
-  end
-  
-  
-  def substractHiddenTreasure (t)
-    
-  end
-  
-  
-  def adjustToFitTreasureLists (v,h)
-    
-  end
-  
-  
-  def to_s
-    "Mal rollo: #{@text} \n Niveles perdidos: #{@levels}, Tesoros visibles: #{@nVisibleTreasures}, Tesoros ocultos: #{@nHiddenTreasures},Tesoros específicos visibles: #{@specificVisibleTreasures}, Tesoros específicos ocultos: #{@specificHiddenTreasures}, Muerto: #{@death}."
-  end
 end
--- NapakalakiR/lib/monster.rb
+++ NapakalakiR/lib/monster.rb
@@ -2,21 +2,27 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-class Monster
-  # Modificadores de Acceso
-  attr_reader :name
-  attr_reader :combatLevel
-  attr_reader :badConsequence
-  attr_reader :prize
+module NapakalakiGame
   
-  def initialize(n,l,b,p)
-    @name = n
-    @combatLevel = l
-    @badConsequence = b
-    @prize = p    
+
+  class Monster
+    # Modificadores de Acceso
+    attr_reader :name
+    attr_reader :combatLevel
+    attr_reader :badConsequence
+    attr_reader :prize
+
+    def initialize(n,l,b,p)
+      @name = n
+      @combatLevel = l
+      @badConsequence = b
+      @prize = p    
+    end
+
+    def to_s
+      "Nombre: #{@name}, CombatLevel: #{@combatLevel} \n  #{@badConsequence.to_s},\n  #{@prize.to_s}"
+    end
   end
-  
-  def to_s
-    "Nombre: #{@name}, CombatLevel: #{@combatLevel} \n  #{@badConsequence.to_s},\n  #{@prize.to_s}"
-  end
+
+
 end
