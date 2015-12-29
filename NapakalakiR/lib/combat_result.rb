77af99e5549387a9b4197146ec3149699465a7e1
joselmo <joselmo@correo.ugr.es>
2015-12-29 18:05:40 +0100

    Mod CombatResult: Añadida opcion Lose and convert

--- NapakalakiR/lib/combat_result.rb
+++ NapakalakiR/lib/combat_result.rb
@@ -4,9 +4,10 @@
 
 module NapakalakiGame
   module CombatResult
-    WINGAME = :wingame
-    WIN     = :win
-    LOSE    = :lose
+    WINGAME         = :wingame
+    WIN             = :win
+    LOSE            = :lose
+    LOSEANDCONVERT  = :loseandconvert
       
   end
 end
