840b335a9212217e6b062440cfc4e8d2fcdaf12c
ElenaMGR <elenamgori@gmail.com>
2016-01-10 17:45:04 +0100

    cambiado to_s

--- NapakalakiR/lib/cultist_player.rb
+++ NapakalakiR/lib/cultist_player.rb
@@ -1,6 +1,7 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+require_relative 'player'
 
 module NapakalakiGame
   class CultistPlayer < Player
@@ -48,7 +49,7 @@ module NapakalakiGame
     
     
     def to_s
-      "Jugador Sectario: " + super
+      "Jugador Sectario: #{getName} \t level: #{getLevels} \t combat level: #{getCombatLevel} \t dead: #{isDead} \t enemy: #{getEnemy.getName}\n" 
     end
     
     private :giveMeATreasure, :canYouGiveMeATreasure
